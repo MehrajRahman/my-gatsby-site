1af05ee1ff6a7041126ecdec1fdb6961959bb57d
unknown <rahmanmehraj627@gmail.com>
2021-08-03 15:08:12 +0600

    Finished Gatsby Tutorial Part 3

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from 'gatsby';
+import Layout from "../components/Layout";
+
+
+function about(props) {
+  return (
+    <div>
+     <Layout pageTitle="About">
+       <p>This is Our About Page</p>
+     </Layout>
+    </div>
+  );
+}
+
+export default about;
